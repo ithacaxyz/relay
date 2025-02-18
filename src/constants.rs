83249153bdafb1366e7a9b90af4489e6d60429ac
Oliver Nordbjerg <hi@notbjerg.me>
2025-02-18 22:08:35 +0100

    temp: bump gas buffer

--- src/constants.rs
+++ src/constants.rs
@@ -11,7 +11,7 @@ pub const INNER_ENTRYPOINT_GAS_OVERHEAD: u64 = 100_000;
 pub const USER_OP_GAS_BUFFER: u64 = 25_000;
 
 /// Extra buffer added to transaction gas estimates to pass the contract 63/64 check.
-pub const TX_GAS_BUFFER: u64 = 50_000;
+pub const TX_GAS_BUFFER: u64 = 1_000_000; // todo: temporarily bumped to 1m from 50k to unblock
 
 /// The EIP-7702 delegation designator.
 pub const EIP7702_DELEGATION_DESIGNATOR: [u8; 3] = hex!("0xef0100");
