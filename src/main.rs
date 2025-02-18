bfa1fa5ff3f218b760d63f718c1d9fc7d90e2f71
Oliver Nordbjerg <hi@notbjerg.me>
2025-02-18 12:05:25 +0100

    chore: init tracing subscriber in main

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@
 //! A relay service that sponsors transactions for EIP-7702 accounts.
 use clap::Parser;
 use relay::cli::*;
+use tracing::level_filters::LevelFilter;
+use tracing_subscriber::{fmt, prelude::*, EnvFilter};
 
 #[tokio::main]
 async fn main() {
@@ -10,6 +12,13 @@ async fn main() {
     if std::env::var_os("RUST_BACKTRACE").is_none() {
         std::env::set_var("RUST_BACKTRACE", "1");
     }
+
+    tracing_subscriber::registry()
+        .with(fmt::layer())
+        .with(
+            EnvFilter::builder().with_default_directive(LevelFilter::INFO.into()).from_env_lossy(),
+        )
+        .init();
 
     let args = Args::parse();
     if let Err(err) = args.run().await {
