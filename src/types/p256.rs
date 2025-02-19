95a8372dbf3d8d640322b55d028c68e87f75eea3
joshieDo <93316087+joshieDo@users.noreply.github.com>
2025-02-19 13:28:18 +0000

    improve sign_with_webauthn

--- src/types/p256.rs
+++ src/types/p256.rs
@@ -3,7 +3,7 @@
 use crate::types::WebAuthnP256;
 use alloy::{
     dyn_abi::Eip712Domain,
-    primitives::{Bytes, B256, U256},
+    primitives::{bytes, Bytes, B256, U256},
     signers::k256::sha2::{Digest, Sha256},
     sol_types::{SolStruct, SolValue},
 };
@@ -55,16 +55,14 @@ impl P256Signer {
 
     /// Returns a signature that the contract can validate.
     fn sign_with_webauthn(&self, digest: B256) -> eyre::Result<Bytes> {
-        // Build authenticatorData
-        let mut authenticator_data = Vec::new();
-        authenticator_data.extend_from_slice(B256::random().as_slice());
-
-        // User Present. User verification is default false, otherwise |= 4
-        let flags: u8 = 0x01;
-        authenticator_data.push(flags);
-
-        // Signature counter
-        authenticator_data.extend_from_slice(&0u32.to_be_bytes());
+        // ID || UserPresent Flag || SignatureCounter
+        let authenticator_data = bytes!(
+            """
+            4242424242424242424242424242424242424242424242424242424242424242
+            01
+            000000
+            """
+        );
 
         // Build clientDataJSON
         let challenge_b64 = base64::engine::general_purpose::URL_SAFE_NO_PAD.encode(digest);
@@ -77,10 +75,10 @@ impl P256Signer {
         let client_data_json = serde_json::to_string(&client_data)?;
 
         // Build digest: SHA256(authenticatorData || SHA256(clientDataJSON))
-        let client_data_hash = Sha256::digest(client_data_json.as_bytes());
-        let mut raw = authenticator_data.clone();
-        raw.extend_from_slice(&client_data_hash);
-        let digest = Sha256::digest(raw);
+        let mut hasher = Sha256::new();
+        hasher.update(&authenticator_data);
+        hasher.update(Sha256::digest(client_data_json.as_bytes()));
+        let digest = hasher.finalize();
 
         // Sign raw data using p256 signing key
         let signature = self.sign(&digest)?;
@@ -90,7 +88,7 @@ impl P256Signer {
         let type_index = U256::from(client_data_json.find("\"type\":").expect("should exist"));
 
         Ok(WebAuthnP256 {
-            authenticatorData: authenticator_data.into(),
+            authenticatorData: authenticator_data,
             clientDataJSON: client_data_json,
             challengeIndex: challenge_index,
             typeIndex: type_index,
