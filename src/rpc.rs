ithacaxyz/relay

commit 47e2f809660cc766c5bce1a01f76601c3fe612b9

feat: execute `eth_call` before broadcasting transaction (#133)
closes https://github.com/ithacaxyz/relay/issues/128

src/rpc.rs
@@ -23,6 +23,7 @@ use alloy::{
     providers::{fillers::NonceManager, Provider},
     rpc::types::{state::AccountOverride, TransactionRequest},
     sol_types::{SolCall, SolValue},
+    transports::TransportErrorKind,
 };
 use futures_util::TryFutureExt;
 use jsonrpsee::{
@@ -47,7 +48,7 @@ use crate::{
     signer::DynSigner,
     types::{
         Account, Action, Entry, EntryPoint, FeeTokens, Key, PartialAction, Quote, SignedQuote,
-        UserOp, U40,
+        UserOp, ENTRYPOINT_NO_ERROR, U40,
     },
 };
 
@@ -393,6 +394,22 @@ impl RelayApiServer for Relay {
                 .await
                 .map_err(|err| SendActionError::InternalError(err.into()))?,
         );
+
+        // try eth_call before committing to send the actual transaction
+        provider
+            .call(&tx)
+            .await
+            .and_then(|res| {
+                EntryPoint::executeCall::abi_decode_returns(&res, true)
+                    .map_err(TransportErrorKind::custom)
+            })
+            .map_err(SendActionError::from)
+            .and_then(|result| {
+                if result.err != ENTRYPOINT_NO_ERROR {
+                    return Err(SendActionError::OpRevert { revert_reason: result.err.into() });
+                }
+                Ok(())
+            })?;
 
         Ok(provider
             .send_raw_transaction(

src/error.rs
@@ -18,7 +18,7 @@ pub enum EstimateFeeError {
     #[error("fee token price not currently available: {0}")]
     UnavailablePrice(Address),
     /// The userop reverted when estimating gas.
-    #[error("op reverted")]
+    #[error("op reverted: {revert_reason}")]
     OpRevert {
         /// The error code returned by the entrypoint.
         revert_reason: Bytes,
@@ -105,6 +105,12 @@ pub enum SendActionError {
     /// The provided quote was not signed by the relay.
     #[error("invalid quote signer")]
     InvalidQuoteSignature,
+    /// The userop reverted when trying transaction.
+    #[error("op reverted: {revert_reason}")]
+    OpRevert {
+        /// The error code returned by the entrypoint.
+        revert_reason: Bytes,
+    },
     /// An error occurred talking to RPC.
     #[error(transparent)]
     RpcError(#[from] alloy::transports::RpcError<alloy::transports::TransportErrorKind>),
@@ -140,7 +146,7 @@ pub enum PriceOracleError {
 #[derive(Debug, thiserror::Error)]
 pub enum CallError {
     /// The userop reverted when estimating gas.
-    #[error("op reverted")]
+    #[error("op reverted: {revert_reason}")]
     OpRevert {
         /// The error code returned by the entrypoint.
         revert_reason: Bytes,

src/types/entrypoint.rs
@@ -14,7 +14,7 @@ use crate::error::CallError;
 use super::UserOp;
 
 /// The 4-byte selector returned by the entrypoint if there is no error during execution.
-const NO_ERROR: FixedBytes<4> = fixed_bytes!("00000000");
+pub const ENTRYPOINT_NO_ERROR: FixedBytes<4> = fixed_bytes!("00000000");
 
 sol! {
     #[sol(rpc)]
@@ -124,7 +124,7 @@ impl<P: Provider> Entry<P> {
                     revert_data.as_deref().unwrap_or(&Bytes::default()),
                     false,
                 ) {
-                    if result.err != NO_ERROR {
+                    if result.err != ENTRYPOINT_NO_ERROR {
                         Err(CallError::OpRevert { revert_reason: result.err.into() })
                     } else {
                         Ok(result.gUsed)
@@ -150,7 +150,7 @@ impl<P: Provider> Entry<P> {
             .await
             .map_err(TransportErrorKind::custom)?;
 
-        if ret.err != NO_ERROR {
+        if ret.err != ENTRYPOINT_NO_ERROR {
             Err(CallError::OpRevert { revert_reason: ret.err.into() })
         } else {
             Ok(())

tests/e2e/cases/simple.rs
@@ -3,11 +3,14 @@
 use crate::e2e::*;
 use alloy::{
     hex,
-    primitives::{Bytes, U256},
+    primitives::{b256, Bytes, B256, U256},
     sol_types::{SolCall, SolValue},
 };
 use eyre::Result;
-use relay::types::{Call, IDelegation::authorizeCall, Key, KeyType};
+use relay::{
+    signer::DynSigner,
+    types::{Call, IDelegation::authorizeCall, Key, KeyType},
+};
 
 #[tokio::test(flavor = "multi_thread")]
 async fn auth_then_erc20_transfer() -> Result<()> {
@@ -64,7 +67,38 @@ async fn invalid_auth_nonce() -> Result<()> {
             .into(),
         }],
         expected: ExpectedOutcome::FailSend,
-        auth: Some(AuthKind::AuthWithNonce(123)),
+        auth: Some(AuthKind::modified_nonce(123)),
+    }];
+
+    run_e2e(test_vector).await
+}
+
+#[tokio::test(flavor = "multi_thread")]
+async fn invalid_auth_signature() -> Result<()> {
+    let test_vector = vec![TxContext {
+        calls: vec![Call {
+            target: EOA_ADDRESS,
+            value: U256::ZERO,
+            data: authorizeCall {
+                key: Key {
+                    expiry: Default::default(),
+                    keyType: KeyType::Secp256k1,
+                    isSuperAdmin: true,
+                    publicKey: EOA_ADDRESS.abi_encode().into(),
+                },
+            }
+            .abi_encode()
+            .into(),
+        }],
+        expected: ExpectedOutcome::FailSend,
+        // Signing with an unrelated key should fail during sendAction when calling eth_call
+        auth: Some(AuthKind::modified_signer(
+            DynSigner::load(
+                "0x42424242428f97a5a0044266f0945389dc9e86dae88c7a8412f4603b6b78690d",
+                None,
+            )
+            .await?,
+        )),
     }];
 
     run_e2e(test_vector).await

tests/e2e/mod.rs
@@ -70,20 +70,8 @@ pub async fn run_e2e(txs: Vec<TxContext>) -> Result<()> {
 ///    - Confirms UserOp success by checking nonce invalidation
 async fn process_tx(nonce: usize, tx: TxContext, env: &Environment) -> Result<()> {
     let execution_data: Bytes = tx.calls.abi_encode().into();
-    let auth = if let Some(auth) = tx.auth {
-        let auth_struct = alloy::eips::eip7702::Authorization {
-            chain_id: U256::from(0),
-            address: env.delegation,
-            nonce: auth.nonce().unwrap_or(nonce as u64),
-        };
-        let auth_hash = auth_struct.signature_hash();
-
-        Some(auth_struct.into_signed(
-            env.eoa_signer.sign_hash(&auth_hash).await.wrap_err("Auth signing failed")?,
-        ))
-    } else {
-        None
-    };
+    let auth =
+        if let Some(auth) = tx.auth { Some(auth.sign(env, nonce as u64).await?) } else { None };
 
     let quote = env
         .relay_endpoint

tests/e2e/types.rs
@@ -1,5 +1,11 @@
-use alloy::sol;
-use relay::types::Call;
+use super::environment::Environment;
+use alloy::{
+    eips::eip7702::SignedAuthorization,
+    primitives::{Address, U256},
+    sol,
+};
+use eyre::WrapErr;
+use relay::{signer::DynSigner, types::Call};
 
 /// Represents the expected outcome of a test case execution
 #[derive(Debug)]
@@ -39,17 +45,52 @@ impl ExpectedOutcome {
 pub enum AuthKind {
     /// Use sequential nonce for authorization
     Auth,
-    /// Use specific nonce value for authorization
-    AuthWithNonce(u64),
+    /// Use specific parameters for authorization
+    ModifiedAuth { signer: Option<DynSigner>, nonce: Option<u64> },
 }
 
 impl AuthKind {
-    /// Return nonce if [`AuthKind::AuthWithNonce`], otherwise `None`
+    /// Return [`AuthKind::ModifiedAuth`] with a specified nonce.
+    pub fn modified_nonce(nonce: u64) -> Self {
+        Self::ModifiedAuth { signer: None, nonce: Some(nonce) }
+    }
+
+    /// Return [`AuthKind::ModifiedAuth`] with a specified signer.
+    pub fn modified_signer(signer: DynSigner) -> Self {
+        Self::ModifiedAuth { signer: Some(signer), nonce: None }
+    }
+
+    /// Return nonce if [`AuthKind::ModifiedAuth`] has it, otherwise `None`
     pub fn nonce(&self) -> Option<u64> {
         match self {
             AuthKind::Auth => None,
-            AuthKind::AuthWithNonce(nonce) => Some(*nonce),
+            AuthKind::ModifiedAuth { signer, nonce } => *nonce,
         }
+    }
+
+    /// Return signer if [`AuthKind::ModifiedAuth`] has it, otherwise `None`
+    pub fn signer(&self) -> Option<&DynSigner> {
+        match self {
+            AuthKind::Auth => None,
+            AuthKind::ModifiedAuth { signer, nonce } => signer.as_ref(),
+        }
+    }
+
+    pub async fn sign(&self, env: &Environment, nonce: u64) -> eyre::Result<SignedAuthorization> {
+        let auth_struct = alloy::eips::eip7702::Authorization {
+            chain_id: U256::from(0),
+            address: env.delegation,
+            nonce: self.nonce().unwrap_or(nonce),
+        };
+        let auth_hash = auth_struct.signature_hash();
+
+        Ok(auth_struct.into_signed(
+            self.signer()
+                .unwrap_or(&env.eoa_signer)
+                .sign_hash(&auth_hash)
+                .await
+                .wrap_err("Auth signing failed")?,
+        ))
     }
 }
 

commit e16051d2ca50bcb47d2904610cdb38a42248fe48

fix: set nonce after eth_call (#141)
if the eth call fails we still bump the nonce internally, introduced in
#133

src/rpc.rs
@@ -392,15 +392,6 @@ impl RelayApiServer for Relay {
             return Err(SendActionError::QuoteExpired.into());
         }
 
-        // broadcast the tx
-        tx.set_nonce(
-            self.inner
-                .nonce_manager
-                .get_next_nonce(&provider, tx_signer_address)
-                .await
-                .map_err(|err| SendActionError::InternalError(err.into()))?,
-        );
-
         // try eth_call before committing to send the actual transaction
         provider
             .call(&tx)
@@ -417,6 +408,14 @@ impl RelayApiServer for Relay {
                 Ok(())
             })?;
 
+        // broadcast the tx
+        tx.set_nonce(
+            self.inner
+                .nonce_manager
+                .get_next_nonce(&provider, tx_signer_address)
+                .await
+                .map_err(|err| SendActionError::InternalError(err.into()))?,
+        );
         Ok(provider
             .send_raw_transaction(
                 &tx.build(&self.inner.tx_signer)