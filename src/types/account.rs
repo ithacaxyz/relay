93bd6736fd56f6492bb0f24f12baddd1e6f309b3
Oliver Nordbjerg <hi@notbjerg.me>
2025-02-26 18:07:43 +0100

    feat: nicks method

--- src/types/account.rs
+++ src/types/account.rs
@@ -1,8 +1,9 @@
 use Delegation::DelegationInstance;
 use alloy::{
-    primitives::Address,
+    eips::eip7702::SignedAuthorization,
+    primitives::{Address, B256, Keccak256, U256},
     providers::Provider,
-    rpc::types::state::StateOverride,
+    rpc::types::{Authorization, state::StateOverride},
     sol,
     transports::{TransportErrorKind, TransportResult},
 };
@@ -80,5 +81,51 @@ impl<P: Provider> Account<P> {
         );
 
         Ok(entrypoint.ENTRY_POINT)
+    }
+}
+
+/// Initializes a new account with the given delegation and digest.
+///
+/// The digest is a hash of the [`UserOp`] used to initialize the account.
+///
+/// This generates a new EOA address and a signed authorization for the account using the Provably
+/// Rootless EIP-7702 Proxy method, which is an application of Nick's Method to EIP-7702.
+///
+/// The authorization item is the signed tuple `(0, delegation, 0)`, where `r` is the hash of the
+/// initialization data, `s` is a random value with a predefined prefix, and `y_parity` is always
+/// `0`.
+///
+/// The `r` value is used as an integrity check in the smart contract to prevent front running, and
+/// the `s` value uses a 20-byte prefix to reasonably prove that the private key is unknown.
+///
+/// Finding the associated private key for the signature would take `2^159` operations given the
+/// predefined prefix (or trillions of years with the most powerful supercomputers available
+/// today).
+///
+/// See <https://blog.biconomy.io/prep-deep-dive/>
+pub fn initialize(delegation: Address, digest: B256) -> (Address, SignedAuthorization) {
+    let random_s: U256 = B256::random().into();
+
+    // we mine until we have a valid `r`, `s` combination
+    let mut i: u64 = 0;
+    loop {
+        let mut hasher = Keccak256::new();
+        hasher.update(digest);
+        hasher.update(i.to_be_bytes());
+        let hash = hasher.finalize();
+
+        let auth = SignedAuthorization::new_unchecked(
+            Authorization { chain_id: U256::ZERO, address: delegation, nonce: 0 },
+            0,
+            hash.into(),
+            // note: the 20 byte prefix more than ensures `s` is always within `secp256kn/2`.
+            random_s >> 160,
+        );
+
+        if let Ok(eoa) = auth.recover_authority() {
+            return (eoa, auth);
+        }
+
+        i += 1;
     }
 }
