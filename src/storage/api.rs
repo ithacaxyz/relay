bcfd62f6391b4819ca38c066bdf00b3f778f6d2e
Oliver Nordbjerg <hi@notbjerg.me>
2025-03-10 16:22:36 +0100

    feat: async storage api

--- src/storage/api.rs
+++ src/storage/api.rs
@@ -3,16 +3,18 @@
 use super::StorageError;
 use crate::types::PREPAccount;
 use alloy::primitives::Address;
+use async_trait::async_trait;
 use std::fmt::Debug;
 
 /// Type alias for `Result<T, StorageError>`
 pub type Result<T> = core::result::Result<T, StorageError>;
 
 /// Storage API.
+#[async_trait]
 pub trait StorageApi: Debug + Send + Sync {
     /// Reads [`PREPAccount`] from storage.
-    fn read_prep(&self, address: &Address) -> Result<Option<PREPAccount>>;
+    async fn read_prep(&self, address: &Address) -> Result<Option<PREPAccount>>;
 
     /// Writes [`PREPAccount`] to storage.
-    fn write_prep(&self, account: &PREPAccount) -> Result<()>;
+    async fn write_prep(&self, account: &PREPAccount) -> Result<()>;
 }
--- src/storage/memory.rs
+++ src/storage/memory.rs
@@ -3,6 +3,7 @@
 use super::{StorageApi, StorageError, api::Result};
 use crate::types::PREPAccount;
 use alloy::primitives::Address;
+use async_trait::async_trait;
 use dashmap::{DashMap, Entry};
 
 /// [`StorageApi`] implementation in-memory. Used for testing
@@ -11,12 +12,13 @@ pub struct InMemoryStorage {
     storage: DashMap<Address, PREPAccount>,
 }
 
+#[async_trait]
 impl StorageApi for InMemoryStorage {
-    fn read_prep(&self, address: &Address) -> Result<Option<PREPAccount>> {
+    async fn read_prep(&self, address: &Address) -> Result<Option<PREPAccount>> {
         Ok(self.storage.get(address).map(|acc| (*acc).clone()))
     }
 
-    fn write_prep(&self, account: &PREPAccount) -> Result<()> {
+    async fn write_prep(&self, account: &PREPAccount) -> Result<()> {
         match self.storage.entry(account.address) {
             Entry::Occupied(_) => Err(StorageError::AccountAlreadyExists(account.address)),
             Entry::Vacant(entry) => {
