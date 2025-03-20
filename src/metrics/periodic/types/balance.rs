fceb981840b6c43f5c2ffc823ebc7abd9b423459
Arsenii Kulikov <klkvrr@gmail.com>
2025-03-20 15:50:44 +0400

    feat: add support for multiple signers in TransactionService

--- src/metrics/periodic/types/balance.rs
+++ src/metrics/periodic/types/balance.rs
@@ -8,43 +8,40 @@ use alloy::{
 use metrics::gauge;
 
 /// This collector queries a chain endpoint for balance of the signer.
+#[derive(Debug)]
 pub struct BalanceCollector<P> {
-    /// Address to be queried.
-    address: Address,
+    /// Addresses to be queried.
+    addresses: Vec<Address>,
     /// Chains endpoints.
     providers_with_chain: Vec<(ChainId, P)>,
 }
 
 impl<P> BalanceCollector<P> {
-    pub fn new(address: Address, providers_with_chain: Vec<(ChainId, P)>) -> Self {
-        Self { address, providers_with_chain }
+    pub fn new(addresses: Vec<Address>, providers_with_chain: Vec<(ChainId, P)>) -> Self {
+        Self { addresses, providers_with_chain }
     }
 }
 
 impl<P> MetricCollector for BalanceCollector<P>
 where
-    P: Provider,
+    P: Provider + Debug,
 {
     async fn collect(&self) -> Result<(), MetricCollectorError> {
-        for (chain_id, provider) in &self.providers_with_chain {
-            let balance = provider.get_balance(self.address).await?;
-
-            gauge!(
-                "balance",
-                "address"  => self.address.to_checksum(Some(*chain_id)),
-                "chain_id" => format!("{chain_id}")
-            )
-            .set::<f64>(balance.into());
+        for address in &self.addresses {
+            futures_util::future::try_join_all(self.providers_with_chain.iter().map(
+                |(chain_id, provider)| async move {
+                    provider.get_balance(*address).await.inspect(|balance| {
+                        gauge!(
+                            "balance",
+                            "address"  => address.to_checksum(Some(*chain_id)),
+                            "chain_id" => format!("{chain_id}")
+                        )
+                        .set::<f64>(balance.into());
+                    })
+                },
+            ))
+            .await?;
         }
         Ok(())
-    }
-}
-
-impl<P> Debug for BalanceCollector<P>
-where
-    P: Provider,
-{
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        f.debug_struct("BalanceCollector").field("address", &self.address).finish()
     }
 }
