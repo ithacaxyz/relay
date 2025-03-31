ab61c98c45349e8258020883e2345024ffb41936
Arsenii Kulikov <klkvrr@gmail.com>
2025-03-31 14:27:15 +0300

    feat: expose EntryPoint contract errors in responses (#314)
    Closes https://github.com/ithacaxyz/relay/issues/148
    
    Adds more `EntryPoint` errors to `sol!` definition if it, and attempts
    to decode the error when returning `OpRevert` variant. If we are able to
    decode error, it's `Debug` representation is returned in response.
    
    I'm wondering if it makes sense to do something better than just
    `Debug`?
    
    ---------
    
    Co-authored-by: joshie <93316087+joshieDo@users.noreply.github.com>

--- src/error/op.rs
+++ src/error/op.rs
@@ -1,7 +1,9 @@
 use super::{internal_rpc, invalid_params, rpc_err};
+use crate::types::{Delegation::DelegationErrors, EntryPoint::EntryPointErrors};
 use alloy::{
     primitives::{B256, Bytes},
     rpc::types::error::EthRpcErrorCode,
+    sol_types::SolInterface,
 };
 use thiserror::Error;
 
@@ -20,11 +22,15 @@ pub enum UserOpError {
         got: B256,
     },
     /// The userop reverted when trying transaction.
-    #[error("op reverted: {revert_reason}")]
-    OpRevert {
-        /// The error code returned by the entrypoint.
-        revert_reason: Bytes,
-    },
+    #[error(transparent)]
+    OpRevert(#[from] OpRevert),
+}
+
+impl UserOpError {
+    /// Creates a new [`UserOpError::OpRevert`] error.
+    pub fn op_revert(revert_reason: Bytes) -> Self {
+        Self::OpRevert(OpRevert::new(revert_reason))
+    }
 }
 
 impl From<UserOpError> for jsonrpsee::types::error::ErrorObject<'static> {
@@ -32,11 +38,57 @@ impl From<UserOpError> for jsonrpsee::types::error::ErrorObject<'static> {
         match err {
             UserOpError::SimulationError => internal_rpc(err.to_string()),
             UserOpError::InvalidOpDigest { .. } => invalid_params(err.to_string()),
-            UserOpError::OpRevert { ref revert_reason } => rpc_err(
-                EthRpcErrorCode::ExecutionError.code(),
-                err.to_string(),
-                Some(revert_reason.clone()),
-            ),
+            UserOpError::OpRevert(err) => err.into(),
         }
+    }
+}
+
+/// An on-chain revert of a userop.
+#[derive(Debug, Error)]
+pub struct OpRevert {
+    /// The returned revert reason bytes.
+    revert_reason: Bytes,
+    /// Decoded revert reason.
+    decoded_error: Option<String>,
+}
+
+impl std::fmt::Display for OpRevert {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let Self { revert_reason, decoded_error } = self;
+        write!(f, "op reverted:")?;
+        if let Some(err) = decoded_error {
+            write!(f, " {err}")
+        } else {
+            write!(f, " {revert_reason}")
+        }
+    }
+}
+
+impl OpRevert {
+    /// Creates a new instance of [`OpRevert`]. Attempts to decode [`EntryPointErrors`]
+    /// and[`DelegationErrors`] .
+    pub fn new(revert_reason: Bytes) -> Self {
+        Self {
+            decoded_error: EntryPointErrors::abi_decode(&revert_reason, false)
+                .ok()
+                .map(|err| format!("{err:?}"))
+                .or_else(|| {
+                    DelegationErrors::abi_decode(&revert_reason, false)
+                        .ok()
+                        .map(|err| format!("{err:?}"))
+                }),
+            revert_reason,
+        }
+    }
+}
+
+impl From<OpRevert> for jsonrpsee::types::error::ErrorObject<'static> {
+    fn from(value: OpRevert) -> Self {
+        let OpRevert { revert_reason, decoded_error } = value;
+        rpc_err(
+            EthRpcErrorCode::ExecutionError.code(),
+            decoded_error.unwrap_or_else(|| revert_reason.to_string()),
+            Some(revert_reason.clone()),
+        )
     }
 }
