0035f56db326cfd8b51d8a95b24ac4f961a9510f
Matthias Seitz <matthias.seitz@outlook.de>
2025-04-07 15:42:42 +0200

    feat: add metrics for price pairs (#367)
    closes #339
    
    adds support for recording price pair metrics

--- src/types/coin.rs
+++ src/types/coin.rs
@@ -1,6 +1,7 @@
 use super::{CoinRegistry, CoinRegistryKey};
 use alloy::primitives::{Address, ChainId};
 use serde::{Deserialize, Serialize};
+use std::fmt::Display;
 
 /// A coin price pair.
 #[derive(Debug, Hash, Eq, PartialEq)]
@@ -15,6 +16,11 @@ impl CoinPair {
     /// Creates a list of ETH coin pairs from a [`CoinKind`] list.
     pub fn ethereum_pairs(coins: &[CoinKind]) -> Vec<Self> {
         coins.iter().map(|coin| CoinPair { from: *coin, to: CoinKind::ETH }).collect()
+    }
+
+    /// Returns the pair identifier.
+    pub fn identifier(&self) -> String {
+        format!("{}/{}", self.from, self.to)
     }
 }
 
@@ -63,5 +69,20 @@ impl CoinKind {
     /// Get [`CoinKind`] from a [`Chain`] and [`Address`].
     pub fn get_token(registry: &CoinRegistry, chain: ChainId, address: Address) -> Option<Self> {
         registry.get(&CoinRegistryKey { chain, token_address: Some(address) }).copied()
+    }
+
+    /// Returns the str identifier
+    pub fn as_str(&self) -> &str {
+        match self {
+            CoinKind::ETH => "ETH",
+            CoinKind::USDT => "USDT",
+            CoinKind::USDC => "USDC",
+        }
+    }
+}
+
+impl Display for CoinKind {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_str(self.as_str())
     }
 }
