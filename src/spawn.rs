e60a67887cbd1a774cf6399a1ba85aa86bebded4
joshie <93316087+joshieDo@users.noreply.github.com>
2025-04-14 19:55:47 +0100

    add delegation proxy to health checkpoint

--- src/spawn.rs
+++ src/spawn.rs
@@ -152,6 +152,7 @@ pub async fn try_spawn(config: RelayConfig, registry: CoinRegistry) -> eyre::Res
     // todo: avoid all this darn cloning
     let rpc = Relay::new(
         config.entrypoint,
+        config.delegation_proxy,
         chains.clone(),
         quote_signer,
         config.quote,
