c69df83231b4aeee0a1ff37b97a9588f7e3e636d
joshieDo <93316087+joshieDo@users.noreply.github.com>
2025-04-15 14:55:16 +0100

    chore: bump account (#472)
    there should be no breaking changes for porto, apart from updating the
    entrypoint & delegation address
    
    the cool thing in this one is that errors are bubbled up during
    simulation now: https://github.com/ithacaxyz/account/pull/112

--- tests/e2e/cases/simple.rs
+++ tests/e2e/cases/simple.rs
@@ -131,7 +131,11 @@ async fn spend_limits() -> Result<()> {
             TxContext {
                 expected: ExpectedOutcome::Pass,
                 authorization_keys: vec![&session_key],
-                calls: vec![calls::daily_limit(env.erc20, U256::from(15), session_key.key())],
+                calls: vec![
+                    calls::daily_limit(env.fee_token, U256::from(1e18), session_key.key()),
+                    calls::can_execute_all(env.erc20, session_key.key_hash()),
+                    calls::daily_limit(env.erc20, U256::from(15), session_key.key()),
+                ],
                 key: Some(&key1),
                 ..Default::default()
             },
