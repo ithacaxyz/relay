31274801acf002987110a88a18d83f0d3669e46e
Arsenii Kulikov <klkvrr@gmail.com>
2025-04-15 21:07:16 +0400

    fix doc

--- src/op.rs
+++ src/op.rs
@@ -10,8 +10,8 @@ pub const OP_FEE_ORACLE_CONTRACT: Address = address!("0x420000000000000000000000
 
 sol! {
     #[sol(rpc)]
-    /// Computes the L1 portion of the fee based on the provided encoded transaction.
     contract OpL1FeeOracle {
+        /// Computes the L1 portion of the fee based on the provided encoded transaction.
         function getL1Fee(bytes memory _data) external view returns (uint256);
     }
 }
