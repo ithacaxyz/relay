350508c24a5d8dff0df29b1385400fab45cd8b8e
Arsenii Kulikov <klkvrr@gmail.com>
2025-04-24 22:47:31 +0400

    chore: speed up stress tests funding (#554)
    Added a simple contract to fund all accounts quickly. It's deployed
    through foundry create2 deployer only if it was not deployed before.
    
    Speeds up funding 500 addresses from 8 minutes to ~1 minute

--- src/types/erc20.rs
+++ src/types/erc20.rs
@@ -9,7 +9,9 @@ sol! {
         function name() external view returns (string);
         function symbol() external view returns (string);
         function decimals() external view returns (uint8);
+        function approve(address spender, uint256 amount) external returns (bool);
         function transfer(address to, uint256 amount) external returns (bool);
         function balanceOf(address eoa) external view returns (uint256);
+        function allowance(address owner, address spender) external view returns (uint256);
     }
 }
