3bee563095f45956ce300f4bd762476b7d343229
joshieDo <93316087+joshieDo@users.noreply.github.com>
2025-04-25 19:11:34 +0100

    Revert "Revert "feat: add specialized preop"" (#553)
    Reverts ithacaxyz/relay#550
    
    merge it whenever, just opening it for context + linking to the
    paymaster design one

--- tests/e2e/cases/calls.rs
+++ tests/e2e/cases/calls.rs
@@ -53,7 +53,7 @@ async fn calls_with_upgraded_account() -> eyre::Result<()> {
             .prepare_calls(PrepareCallsParameters {
                 calls: vec![erc20_transfer.clone()],
                 chain_id: env.chain_id,
-                from: env.eoa.address(),
+                from: Some(env.eoa.address()),
                 capabilities: PrepareCallsCapabilities {
                     authorize_keys: Vec::new(), // todo: add test authorize "inline"
                     revoke_keys: Vec::new(),
