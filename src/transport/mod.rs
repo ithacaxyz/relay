f6cede88b374d39a52e29c19c24e0a0b2af75312
Arsenii Kulikov <klkvrr@gmail.com>
2025-05-15 00:29:10 +0400

    fix: correctly handle errors in `SequencerService` (#647)
    We should handle the inner failing JSON-RPC responses vs the errors
    returned by transport

--- src/transport/mod.rs
+++ src/transport/mod.rs
@@ -1,6 +1,5 @@
 //! L2 Transport implementation with `eth_sendRawTransaction` forwarding.
 
-use crate::transport::error::TransportErrExt;
 use alloy::{
     rpc::json_rpc::{RequestPacket, ResponsePacket},
     transports::{Transport, TransportError, TransportFut},
@@ -71,27 +70,30 @@ where
                 let to_sequencer = self.sequencer.call(r.clone().into());
                 let to_inner = self.inner.call(req);
                 return Box::pin(async move {
-                    let (seq, inner) = futures_util::future::join(to_sequencer, to_inner).await;
+                    let (seq, inner) =
+                        futures_util::future::try_join(to_sequencer, to_inner).await?;
 
                     // Handle potential errors. We are not treating "already known" as fatal if at
                     // least one of the endpoints accepted the transaction
-                    match (seq, inner) {
-                        (Ok(seq), _) => Ok(seq),
-                        (Err(seq), Ok(inner)) => {
-                            if seq.is_already_known() {
-                                Ok(inner)
+                    let resp = match (seq.as_error(), inner.as_error()) {
+                        (None, _) => seq,
+                        (Some(err), None) => {
+                            if err.message == "already known" {
+                                inner
                             } else {
-                                Err(seq)
+                                seq
                             }
                         }
-                        (Err(seq), Err(inner)) => {
-                            if seq.is_already_known() {
-                                Err(inner)
+                        (Some(err), Some(_)) => {
+                            if err.message == "already known" {
+                                inner
                             } else {
-                                Err(seq)
+                                seq
                             }
                         }
-                    }
+                    };
+
+                    Ok(resp)
                 });
             }
         }
