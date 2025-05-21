b4cecc6ce46becf3dad1b3be7423252f42490bbb
joshieDo <93316087+joshieDo@users.noreply.github.com>
2025-05-21 12:11:30 +0100

    sql

--- migrations/0006_entrypoint.sql
+++
@@ -1,2 +0,0 @@
-update pending_txs set tx = jsonb_set(tx, '{quote,entrypoint}', tx->'entrypoint') - 'entrypoint';
-update queued_txs set tx = jsonb_set(tx, '{quote,entrypoint}', tx->'entrypoint') - 'entrypoint';
