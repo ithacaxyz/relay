7a027c2be829c5b9be60f7bf44327ea5280c90c6
Arsenii Kulikov <klkvrr@gmail.com>
2025-06-25 22:31:33 +0300

    fix

--- src/spawn.rs
+++ src/spawn.rs
@@ -103,10 +103,9 @@ pub async fn try_spawn_with_args<P: AsRef<Path>>(
 }
 
 /// Spawns the relay service using the provided [`RelayConfig`] and [`CoinRegistry`].
-pub async fn try_spawn(
-    config: RelayConfig,
-    mut registry: CoinRegistry,
-) -> eyre::Result<RelayHandle> {
+pub async fn try_spawn(config: RelayConfig, registry: CoinRegistry) -> eyre::Result<RelayHandle> {
+    let registry = Arc::new(registry);
+
     // construct db
     let storage = if let Some(ref db_url) = config.database_url {
         info!("Using PostgreSQL as storage.");
@@ -162,11 +161,6 @@ pub async fn try_spawn(
     )
     .await?;
 
-    let chains = Chains::new(providers.clone(), signers, storage.clone(), &config).await?;
-
-    registry.retain(|k, _| chains.get(k.chain).is_some());
-    let registry = Arc::new(registry);
-
     // setup metrics exporter and periodic metric collectors
     let metrics =
         metrics::setup_exporter((config.server.address, config.server.metrics_port)).await;
@@ -193,6 +187,8 @@ pub async fn try_spawn(
             &CoinPair::ethereum_pairs(&[CoinKind::USDT, CoinKind::USDC]),
         );
     }
+
+    let chains = Chains::new(providers.clone(), signers, storage.clone(), &config).await?;
 
     // construct asset info service
     let asset_info = AssetInfoService::new(512);
