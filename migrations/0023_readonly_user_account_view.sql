b4a41c523ffcdbced94b5e700ab5fb3f6444f392
o-az <omaraziz.dev@proton.me>
2025-08-04 11:41:30 -0700

    chore: comment

--- migrations/0023_readonly_user_account_view.sql
+++ migrations/0023_readonly_user_account_view.sql
@@ -1,4 +1,3 @@
--- Create a view that shows account address, email (if exists), and verification status
 CREATE OR REPLACE VIEW account_emails_view AS
 SELECT 
     encode(a.address, 'hex') as account_address,
@@ -11,8 +10,8 @@ SELECT
 FROM accounts a
 LEFT JOIN emails e ON a.address = e.address;
 
--- Create readonly user (Note: password should be changed in production)
--- The user creation might fail if it already exists, so we use DO block for conditional creation
+-- (Note: password should be changed in production)
+-- DO block for conditional creation (in case user already exists)
 DO $$
 BEGIN
     IF NOT EXISTS (SELECT FROM pg_catalog.pg_roles WHERE rolname = 'readonly_user') THEN
@@ -28,10 +27,8 @@ REVOKE ALL ON ALL TABLES IN SCHEMA public FROM readonly_user;
 REVOKE ALL ON ALL SEQUENCES IN SCHEMA public FROM readonly_user;
 REVOKE ALL ON ALL FUNCTIONS IN SCHEMA public FROM readonly_user;
 
--- Grant connect to database
 GRANT CONNECT ON DATABASE postgres TO readonly_user;
 
--- Grant usage on schema
 GRANT USAGE ON SCHEMA public TO readonly_user;
 
 -- Grant SELECT permission ONLY on the account_emails_view
