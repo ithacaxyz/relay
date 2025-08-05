7ec809d39f700892fded883838b084d211640adf
onbjerg <onbjerg@users.noreply.github.com>
2025-08-05 16:00:15 -0700

    refactor: rm unused constants (#1037)

--- src/constants.rs
+++ src/constants.rs
@@ -23,37 +23,6 @@ pub const DEFAULT_RPC_DEFAULT_MAX_CONNECTIONS: u32 = 5_000;
 /// Extra buffer added to transaction gas estimates to pass the contract 63/64 check.
 pub const TX_GAS_BUFFER: u64 = 1_000_000; // todo: temporarily bumped to 1m from 50k to unblock
 
-/// The Base Mainnet sequencer URL.
-///
-/// This is only suitable for submitted transactions via `eth_sendRawTransaction`, all other methods
-/// are disabled.
-pub const BASE_MAINNET_SEQUENCER_URL: &str = "https://mainnet-sequencer.base.org";
-
-/// The Base Sepolia sequencer URL.
-///
-/// This is only suitable for submitted transactions via `eth_sendRawTransaction`, all other methods
-/// are disabled.
-pub const BASE_SEPOLIA_SEQUENCER_URL: &str = "https://sepolia-sequencer.base.org";
-
-/// The Base Sepolia URL for flashblocks: <https://docs.base.org/chain/flashblocks#rpc-api>
-///
-/// Supports:
-///   - `eth_getTransactionReceipt`: returns (pre-)confirmed receipt.
-///   - `eth_getBlockByNumber`: returns the latest flashblock.
-pub const BASE_SEPOLIA_PRECONF_HTTP_URL: &str = "https://sepolia-preconf.base.org";
-
-/// The public Base Mainnet RPC URL.
-///
-/// This endpoint is rate-limited.
-/// See also <https://docs.base.org/chain/network-information>
-pub const BASE_MAINNET_PUBLIC_RPC_URL: &str = "https://mainnet.base.org";
-
-/// The public Base Sepolia RPC URL.
-///
-/// This endpoint is rate-limited.
-/// See also <https://docs.base.org/chain/network-information>
-pub const BASE_SEPOLIA_PUBLIC_RPC_URL: &str = "https://sepolia.base.org";
-
 /// Default cap on maximum number of pending transactions per chain.
 pub const DEFAULT_MAX_TRANSACTIONS: usize = 100;
 
