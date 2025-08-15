9c851995d6caa57a281a62e564b5afd7c1a64a49
Matthias Seitz <matthias.seitz@outlook.de>
2025-08-15 18:19:46 +0200

    test: add sanity bsc 1559 tests (#1132)
    towards #1128
    
    ensure that we receive correct feehistory responses

--- src/estimation/fees.rs
+++ src/estimation/fees.rs
@@ -41,6 +41,10 @@ pub fn approx_intrinsic_cost(input: &[u8], has_auth: bool) -> u64 {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use alloy::{
+        providers::utils::{Eip1559Estimation, Eip1559Estimator},
+        rpc::types::FeeHistory,
+    };
 
     #[test]
     fn test_approx_intrinsic_cost_without_auth() {
@@ -70,5 +74,157 @@ mod tests {
         // Test with large calldata
         let calldata = vec![0xff; 1000];
         assert_eq!(approx_intrinsic_cost(&calldata, false), 21000 + 1000 * 16);
+    }
+
+    // feehistory response public bsc endpoint <https://bsc-dataseed.bnbchain.org>
+    #[test]
+    fn fee_history_bsc_public_bsc() {
+        // for [
+        // 		10,
+        // 		"latest",
+        //       [20.0]
+        // 	],
+        let sample = r#"{
+        "oldestBlock": "0x3706d1a",
+        "reward": [
+            [
+                "0x5f5e100"
+            ],
+            [
+                "0x5f5e100"
+            ],
+            [
+                "0x64"
+            ],
+            [
+                "0x5f5e100"
+            ],
+            [
+                "0x5f5e100"
+            ],
+            [
+                "0x5f5e100"
+            ],
+            [
+                "0x5f5e100"
+            ],
+            [
+                "0x5f5e100"
+            ],
+            [
+                "0x5f5e100"
+            ],
+            [
+                "0x5f5e100"
+            ]
+        ],
+        "baseFeePerGas": [
+            "0x0",
+            "0x0",
+            "0x0",
+            "0x0",
+            "0x0",
+            "0x0",
+            "0x0",
+            "0x0",
+            "0x0",
+            "0x0",
+            "0x0"
+        ],
+        "gasUsedRatio": [
+            0.28533176,
+            0.218322,
+            0.15909398666666666,
+            0.17449282666666666,
+            0.22352961333333332,
+            0.26406174666666665,
+            0.28206398666666666,
+            0.20695654666666666,
+            0.22779266666666667,
+            0.23267304
+        ],
+        "baseFeePerBlobGas": [
+            "0x1",
+            "0x1",
+            "0x1",
+            "0x1",
+            "0x1",
+            "0x1",
+            "0x1",
+            "0x1",
+            "0x1",
+            "0x1",
+            "0x1"
+        ],
+        "blobGasUsedRatio": [
+            0,
+            0.16666666666666666,
+            0,
+            0,
+            0,
+            0.16666666666666666,
+            0,
+            0,
+            0,
+            0.16666666666666666
+        ]
+    }"#;
+
+        let fee_history = serde_json::from_str::<FeeHistory>(sample).unwrap();
+
+        let last_base_fee = fee_history.latest_block_base_fee().unwrap_or_default();
+
+        let fee_estimate = Eip1559Estimator::default()
+            .estimate(last_base_fee, &fee_history.reward.unwrap_or_default());
+        // ensure these return 1gwei
+        assert_eq!(
+            fee_estimate,
+            Eip1559Estimation { max_fee_per_gas: 100000000, max_priority_fee_per_gas: 100000000 }
+        )
+    }
+
+    // feehistory from pro quiknode
+    #[test]
+    fn fee_history_bsc_quiknode() {
+        // for [
+        // 		10,
+        // 		"latest",
+        //       [20.0]
+        // 	],
+        let sample = r#"{
+        "oldestBlock": "0x3706ed7",
+        "reward": [
+            [
+                "0x5f5e100"
+            ]
+        ],
+        "baseFeePerGas": [
+            "0x0",
+            "0x0"
+        ],
+        "gasUsedRatio": [
+            0.20435928
+        ],
+        "baseFeePerBlobGas": [
+            "0x1",
+            "0x1"
+        ],
+        "blobGasUsedRatio": [
+            0.16666666666666666
+        ]
+    }"#;
+
+        let fee_history = serde_json::from_str::<FeeHistory>(sample).unwrap();
+
+        let last_base_fee = fee_history.latest_block_base_fee().unwrap_or_default();
+
+        let fee_estimate = Eip1559Estimator::default()
+            .estimate(last_base_fee, &fee_history.reward.unwrap_or_default());
+
+        // ensure these return 1gwei
+        assert_eq!(
+            fee_estimate,
+            Eip1559Estimation { max_fee_per_gas: 100000000, max_priority_fee_per_gas: 100000000 }
+        )
     }
 }
