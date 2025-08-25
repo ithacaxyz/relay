e09e7a543b547f1f512f8741b50efb17c62ed894
Dan Cline <6798349+Rjected@users.noreply.github.com>
2025-08-25 16:28:55 -0400

    feat(relay): add extra fee info with is_paid_in_gas

---
+++ src/rpc/extra_fee.rs
@@ -0,0 +1,34 @@
+//! Extra fee info for L2s
+use alloy::primitives::U256;
+
+/// Contains information about the extra fee on L2 transactions, these come from costs that the L2
+/// incurs when posting the transaction on the parent chain.
+#[derive(Debug)]
+pub struct ExtraFeeInfo {
+    /// The extra fee
+    /// TODO: add units to docs
+    extra_fee: U256,
+    /// Whether or not the fee should be part of the gas limit.
+    ///
+    /// On optimism chains, the extra fee is deducted from the account directly and does not need
+    /// to be included in the gas limit for the transaction:
+    /// https://docs.optimism.io/stack/transactions/fees#mechanism
+    ///
+    /// On arbitrum chains, the extra fee is also required to be added to the gas limit for the
+    /// transaction:
+    /// https://docs.arbitrum.io/build-decentralized-apps/how-to-estimate-gas#breaking-down-the-formula
+    is_paid_in_gas: bool,
+}
+
+impl ExtraFeeInfo {
+    /// Creates the extra fee info struct. The parameter `is_arbitrum` determines the
+    /// `is_paid_in_gas` field, because arbitrum requires extra fee to be paid in gas.
+    pub fn new(extra_fee: U256, is_arbitrum: bool) -> Self {
+        Self { extra_fee, is_paid_in_gas: is_arbitrum }
+    }
+
+    /// Returns the extra fee
+    pub fn extra_fee(&self) -> U256 {
+        self.extra_fee
+    }
+}
--- src/rpc/mod.rs
+++ src/rpc/mod.rs
@@ -1,7 +1,9 @@
 //! RPC modules.
 
 mod account;
+mod extra_fee;
 mod relay;
 
 pub use account::*;
+pub use extra_fee::*;
 pub use relay::*;
